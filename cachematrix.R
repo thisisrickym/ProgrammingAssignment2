708655ce8d6b724a9e081cd107e9633d2d683061
thisisrickym <mailhotrl@gmail.com>
2016-05-16 20:54:50 -0700

    Program for grading

--- cachematrix.R
+++ cachematrix.R
@@ -1,15 +1,39 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Used together these two functions create a cache for a matrix's inverse.
 
-## Write a short comment describing this function
+## The first function creates a list for your inputed function. This list
+## is basically a function to
+## 1. Set the value of the matrix
+## 2. Get the value of the matrix
+## 3. Set the value of the inverse of the matrix
+## 4. Get the value of the inverse of the matrix
 
 makeCacheMatrix <- function(x = matrix()) {
-
+    m <- NULL
+    set <- function(y) {
+        x <<- y
+        m <<- NULL
+    }
+    get <- function() x
+    setinv <- function(solve) m <<- solve
+    getinv <- function() m
+    list(set = set, get = get, setinv = setinv, getinv = getinv)
 }
 
 
-## Write a short comment describing this function
+## This second function takes the output of the above function and computes
+## your original matrix's inverse.  The function checks to see if the inverse
+## has already been calculated. If so it returns the inverse from the cache
+## and skips any further computation. Otherwise it calculates the inverse of
+## the matrix and sets the inverse in the cache via the setinv function.
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
+    m <- x$getinv()
+    if(!is.null(m)){
+        message("getting cached data")
+        return(m)
+    }
+    data <- x$get()
+    m <- solve(data, ...)
+    x$setinv(m)
+    m
 }
